bf77643720658d30e4b084a5c8fd983c7568a8f3
Mario Alberto <mario@novomedia.mx>
2022-01-10 18:36:19 -0600

    Nueva pantalla Home

--- generacion-x/index.php
+++ generacion-x/index.php
@@ -9,86 +9,71 @@
         <link rel="stylesheet" href="css/styles.css">
     </head>
     
-    <body class="back1">
-        <div class="container mt-5">
+    <body>
+        <div class="container-fluid backsquare1">
             <div class="row">
-                <div class="col-12 col-lg-8 offset-lg-2">
-                    <iframe class="MainVideo" width="100%" src="https://www.youtube.com/embed/lrzno3Pg3RI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>                                                              
+                <div class="col-lg-4 offset-lg-4 my-4 text-center">
+                    <img src="images/logoMK.png" class="img-fluid" alt="">
+                </div>
+                <div class="col-12 col-lg-6 offset-lg-3">
+                    <iframe class="MainVideo" width="100%" src="https://www.youtube.com/embed/rhlKpvoGBgU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div> 
+                <div class="col-8 offset-2 col-lg-4 offset-lg-4 my-4">
+                    <img src="images/logo-inicia.png" class="img-fluid" alt="">
+                </div> 
+                <div class="col-12 col-lg-6 offset-lg-3 mt-4">
+                    <p class="white txt-responsive strong text-center" style='font-family: "MaryKaySans-Medium";'>
+                        <span class="pink">Enero es el mejor mes para cambiar tu vida.</span> En Mary Kay queremos
+                        ayudarte a iniciar tu propio Negocio Independiente y construir tu camino
+                        al éxito. <span class="pink">Conoce las opciones de Kits de Inicio que éste mes tenemos para ti.</span>
+                    </p>
+                    
+                    <img src="images/main-kit.png" class="img-fluid" alt="">
+                </div>                                                            
             </div>
         </div>   
         
-        <div class="container d-lg-none">
-            <div class="row">
-                <div class="col-8 offset-2 col-lg-6 offset-lg-3 my-4 d-none d-lg-block">
-                    <img src="images/logo-inicia.png" class="img-fluid" alt="">
-                </div>
-                               
-                <div class="col-12 col-lg-6 offset-lg-3 text-center my-4">
-                    <p class="white text-center">
-                        Descubre tu comienzo como Consultora de Belleza Independiente Mary Kay® y ¡cambia tu vida por completo! 
-                    </p>
-                </div>
-                
-                <div class="col-12 col-lg-6 offset-lg-3 text-center">
-                    <img src="images/kit1.png" class="img-fluid" alt="">                    
-                </div>  
-            </div>
+        <div class="container-fluid purple-line text-center py-3">
+            A mayor inversión <strong>MAYOR</strong> descuento y <strong>MÁS</strong> productos
         </div>
         
-        <div class="container d-none d-lg-block p-0">
-            <div class="row my-5">
-                <div class="col-lg-6 p-5">
-                    <img src="images/logo-inicia.png" class="img-fluid" alt="">
-                    <p class="white text-center">
-                        Descubre tu comienzo como Consultora de Belleza Independiente Mary Kay® y ¡cambia tu vida por completo! 
-                    </p>
-                </div>
-                <div class="col-lg-6 text-end">
-                    <img src="images/kit3.png" class="img-fluid" alt="">
-                </div>
-            </div>
-            
+        <div class="container-fluid backsquare2">
             <div class="row">
-                <div class="col-6 offset-lg-3 p-5 text-center">
-                    <img src="images/logo-inicia.png" class="img-fluid" alt="">
+                <div class="col-12 col-lg-12 text-center my-5">
+                    <h2 class="main-tit2 white">
+                        <span class="pink">¡AHORA ES TU MOMENTO</span><br> 
+                        PARA INICIAR!
+                    </h2>
                 </div>
-            </div>
-        </div>
-        
-        <div class="container">
-            <div class="row">
-                <div class="col-12 p-5 text-center d-lg-none">
-                    <img src="images/logo-inicia.png" class="img-fluid" alt="">
-                </div>
-                
-                <div class="col-lg-6 offset-lg-3 col-12">
-                    <p class="white text-center">
-                        <strong>Este Enero Rosa,</strong> queremos ayudarte a <strong>iniciar de la mejor manera,</strong> porque es momento de comenzar a <strong>crear tu propio camino,</strong> de amar lo que haces y lograr el éxito profesional para <strong>ser la persona que siempre soñaste ser.</strong> 
+                <div class="col-12 col-lg-6 offset-lg-3 mb-5">
+                    <p class="white txt-responsive strong text-center">
+                        Estás a solo un clic de iniciar tu nueva vida.<br>
+                        Regístrate y una Consultora de Ventas Independiente Mary Kay<br>
+                        te guiará paso a paso en tu camino al éxito.
                     </p>
                 </div>
                 
-                <div class="col-12 col-lg-6 text-center my-4">                                        
+                <div class="col-lg-4 offset-lg-4 col-12 col-sm-10 offset-sm-1 form-container p-4">
                     <form action="" id="MainForm">
-                        <div class="mb-3 mt-5 text-start">
-                            <label for="nombre" class="form-label">
-                                Nombre Completo
+                        <div class="mb-3 text-start">
+                            <label for="nombre" class="form-label mb-0">
+                                <img src="images/label_nombre.png" class="img-fluid" alt="">
                             </label>
                             <input type="text" class="form-control" id="nombre" name="nombre" placeholder="*Ingresa tu nombre completo como aparece en tu documento oficial. ">    
                         </div>
                         <div class="mb-3 text-start">
-                            <label for="email" class="form-label">
-                                Correo Electrónico
+                            <label for="email" class="form-label mb-0">
+                                <img src="images/label_email.png" class="img-fluid" alt="">
                             </label>
                             <input type="text" class="form-control" id="email" name="email" placeholder="*Ingresa tu correo electrónico">    
                         </div>                        
                         <div class="mb-3 text-start">
-                            <label for="cp" class="form-label">
-                                Código Postal
+                            <label for="cp" class="form-label mb-0">
+                                <img src="images/label_cp.png" class="img-fluid" alt="">
                             </label>
                             <input type="number" class="form-control" id="cp" name="cp" placeholder="*Ingresa tu Código Postal." minlength="5" maxlength="5" oninput="if(this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);">    
                         </div>
-                        <div class="mb-3">
+                        <div class="mb-3 text-center">
                             <div class="form-check form-check-inline">
                                 <input class="form-check-input" type="radio" name="genero" id="hombre" value="option1">
                                 <label class="form-check-label" for="hombre">Hombre</label>
@@ -109,21 +94,19 @@
                                     Confirmo que he leído y acepto el aviso de privacidad y los términos y condiciones.  
                                 </label>
                             </div>
-                        </div>
-                        <div class="mb-3 text-center">
-                            <button id="FormSubmit" class="shadow-none">
-                                <img src="images/btn-enviar.png" class="img-fluid" alt="" style="width: 100px;">
-                            </button>
-                        </div>
+                        </div>                        
                     </form>
                 </div>
                 
-                <div class="col-12 col-lg-6 text-center mt-4 d-none d-lg-block">
-                    <img src="images/kit4.png" class="img-fluid" alt="">
+                <div class="col-lg-4 offset-lg-4 col-12 text-center my-5">
+                    <button id="FormSubmit" class="shadow-none">
+                        <!-- <img src="images/btn-enviar.png" class="img-fluid" alt="" style="width: 100px;"> -->
+                        ENVIAR
+                    </button>
                 </div>
             </div>
         </div>
-        
+                
         <footer class="container-fluid py-2" style="background-color: #e60f76;">
             <div class="row">
                 <div class="col-12 text-center white" style="font-size: 10px;">
